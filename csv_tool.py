a2b81d101c77b5234558f0511e3e21053425b79e
Tomas Hluchan <tomas.hluchan@gmail.com>
2014-01-28 14:55:02 +0100

    tests

---
+++ csv_tool.py
@@ -0,0 +1,85 @@
+import csv,codecs,cStringIO
+import logging
+
+
+class UnicodeWriter:
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        orow = []
+        for s in row:
+            if 'encode' in dir(s):
+                orow.append(s.encode("utf-8"))
+            else:
+                orow.append(s)
+            
+        self.writer.writerow(orow)
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+
+
+def dump_to_csv(data,out):
+    wr = UnicodeWriter(out,delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)        
+    for row in data:
+#        logging.info(row)
+        wr.writerow(row)
+ 
+def read_csv(f, X):
+    ret = []
+    rr = UnicodeReader(f,encoding='1250', delimiter=';', quotechar='"')
+    for row in rr:
+        logging.info(row) 
+
+
+
+
+
+
--- hello.py
+++ hello.py
@@ -1,31 +1,32 @@
 from flask import Flask
 from flask import request
+from flask import render_template
     
-
+from csv_tool import UnicodeReader
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/up', methods=['GET', 'POST'])
 def upload_file():
+    data = None
     if request.method == 'POST':
         file = request.files['file']
         app.logger.debug('file name %s' % file.filename)
 
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+        ret = []
+        rr = UnicodeReader(file,encoding='utf8', delimiter=';', quotechar='"')
+        data = []
+        for row in rr:
+           app.logger.debug(row)
+           data.append(row[0])
+
+    return render_template('up.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
